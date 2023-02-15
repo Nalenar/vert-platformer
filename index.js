02254a3162a5e1a56377a1dcbe411ceaba8ecebf
Romich <nichrom.isme@gmail.com>
2023-02-15 17:50:54 +0300

    Added movement to a player

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ class Player {
     this.velocity = { x: 0, y: 0 };
     this.width = 100;
     this.height = 100;
+    this.lastKey;
   }
 
   draw() {
@@ -37,12 +38,59 @@ const player = new Player({
   position: { x: 200, y: 100 },
 });
 
+const keys = {
+  a: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
+};
+
 function animate() {
   window.requestAnimationFrame(animate);
   c.fillStyle = "#525252";
   c.fillRect(0, 0, canvas.width, canvas.height);
 
   player.update();
+
+  player.velocity.x = 0;
+  if (keys.a.pressed && player.lastKey === "a") {
+    player.velocity.x = -4;
+  } else if (keys.d.pressed && player.lastKey === "d") {
+    player.velocity.x = 4;
+  }
 }
 
 animate();
+
+window.addEventListener("keydown", (event) => {
+  switch (event.key) {
+    case "a":
+    case "ф":
+      keys.a.pressed = true;
+      player.lastKey = "a";
+      break;
+    case "d":
+    case "в":
+      keys.d.pressed = true;
+      player.lastKey = "d";
+      break;
+    case "w":
+    case "ц":
+      player.velocity.y = -10;
+  }
+});
+
+window.addEventListener("keyup", (event) => {
+  switch (event.key) {
+    case "a":
+    case "ф":
+      keys.a.pressed = false;
+      break;
+    case "d":
+    case "в":
+      keys.d.pressed = false;
+      break;
+  }
+});
