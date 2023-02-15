fe2c2438df0166ff341aa2d906995e41d8e900c3
Romich <nichrom.isme@gmail.com>
2023-02-15 17:39:55 +0300

    Added gravity

--- index.js
+++ index.js
@@ -1,13 +1,48 @@
-const canvas = document.querySelector('canvas');
-const c = canvas.getContext('2d');
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
 
-canvas.width = 1024
-canvas.height = 576
+canvas.width = 1024;
+canvas.height = 576;
 
-function animate() {
-    window.requestAnimationFrame(animate)
-    c.fillStyle = '#525252'
-    c.fillRect(0, 0, canvas.width, canvas.height)
+const gravity = 1;
+
+class Player {
+  constructor({ position }) {
+    this.position = position;
+    this.velocity = { x: 0, y: 0 };
+    this.width = 100;
+    this.height = 100;
+  }
+
+  draw() {
+    c.fillStyle = "red";
+    c.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+
+  update() {
+    this.draw();
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+
+    // gravity function
+    if (this.position.y + this.velocity.y + this.height <= canvas.height) {
+      this.velocity.y += gravity;
+    } else {
+      this.velocity.y = 0;
+    }
+  }
 }
 
-animate()
+const player = new Player({
+  position: { x: 200, y: 100 },
+});
+
+function animate() {
+  window.requestAnimationFrame(animate);
+  c.fillStyle = "#525252";
+  c.fillRect(0, 0, canvas.width, canvas.height);
+
+  player.update();
+}
+
+animate();
